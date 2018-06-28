46d4c3cc815acc3a55e636f37614f8294f6f56c3
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2018-06-28 15:55:18 +0200

    import: add several new data resources for new search classes

--- scripts/geocodr-pg2csv.sh
+++ scripts/geocodr-pg2csv.sh
@@ -128,6 +128,137 @@ FROM ${DBSCHEMA}.flurstuecke_alle
 END
 )"
 
+pg2csv $CSV_OUTDIR/gemeinden_hro.csv "$(cat << END
+COPY (SELECT
+  uuid AS id,
+  ST_AsText(ST_Buffer(ST_Simplify(geometrie, 5), 0)) AS geometrie,
+  gemeinde_name,
+  to_jsonb(gemeinden) - 'geometrie' AS json
+FROM ${DBSCHEMA}.gemeinden WHERE kreis_schluessel = '13003'
+) TO STDOUT WITH CSV HEADER;
+END
+)"
+
+
+pg2csv $CSV_OUTDIR/gemeindeteile_hro.csv "$(cat << END
+COPY (SELECT
+  uuid AS id,
+  ST_AsText(ST_Buffer(ST_Simplify(geometrie, 5), 0)) AS geometrie,
+  gemeinde_name,
+  gemeindeteil_name,
+  to_jsonb(gemeindeteile) - 'geometrie' AS json
+FROM ${DBSCHEMA}.gemeindeteile WHERE kreis_schluessel = '13003'
+) TO STDOUT WITH CSV HEADER;
+END
+)"
+
+
+pg2csv $CSV_OUTDIR/strassen_hro.csv "$(cat << END
+COPY (SELECT
+  row_number() OVER () AS id,
+  ST_AsText(ST_Simplify(geometrie, 1)) AS geometrie,
+  gemeinde_name,
+  gemeindeteil_name,
+  strasse_name,
+  to_jsonb(strassen_mit_gemeindeteil) - 'geometrie' AS json
+FROM ${DBSCHEMA}.strassen_mit_gemeindeteil
+) TO STDOUT WITH CSV HEADER;
+END
+)"
+
+pg2csv $CSV_OUTDIR/adressen_hro.csv "$(cat << END
+COPY (SELECT
+  uuid AS id,
+  ST_AsText(geometrie) AS geometrie,
+  strasse_name,
+  postleitzahl,
+  gemeindeteil_name,
+  gemeinde_name,
+  hausnummer AS hausnummer_int,
+  hausnummer || coalesce(hausnummer_zusatz, '') AS hausnummer,
+  to_jsonb(adressen) - 'geometrie' AS json
+FROM ${DBSCHEMA}.adressen
+) TO STDOUT WITH CSV HEADER;
+END
+)"
+
+pg2csv $CSV_OUTDIR/adressen_historisch_hro.csv "$(cat << END
+COPY (SELECT
+  uuid AS id,
+  ST_AsText(geometrie) AS geometrie,
+  strasse_name,
+  postleitzahl,
+  gemeindeteil_name,
+  gemeinde_name,
+  hausnummer AS hausnummer_int,
+  hausnummer || coalesce(hausnummer_zusatz, '') AS hausnummer,
+  to_jsonb(adressen_historisch) - 'geometrie' AS json
+FROM ${DBSCHEMA}.adressen_historisch
+) TO STDOUT WITH CSV HEADER;
+END
+)"
+
+pg2csv $CSV_OUTDIR/gemarkungen_hro.csv "$(cat << END
+COPY (SELECT
+  uuid AS id,
+  ST_AsText(ST_Buffer(ST_Simplify(geometrie, 5), 0)) AS geometrie,
+  gemarkung_name,
+  gemarkung_schluessel,
+  gemeinde_name,
+  to_jsonb(gemarkungen) - 'geometrie' AS json
+FROM ${DBSCHEMA}.gemarkungen
+) TO STDOUT WITH CSV HEADER;
+END
+)"
+
+pg2csv $CSV_OUTDIR/fluren_hro.csv "$(cat << END
+COPY (SELECT
+  uuid AS id,
+  ST_AsText(ST_Buffer(ST_Simplify(geometrie, 1), 0)) AS geometrie,
+  gemarkung_name,
+  gemarkung_schluessel,
+  gemeinde_name,
+  flur,
+  to_jsonb(fluren) - 'geometrie' AS json
+FROM ${DBSCHEMA}.fluren
+) TO STDOUT WITH CSV HEADER;
+END
+)"
+
+pg2csv $CSV_OUTDIR/flurstuecke_hro.csv "$(cat << END
+COPY (SELECT
+  uuid AS id,
+  ST_AsText(ST_Buffer(ST_Simplify(geometrie, 0.5), 0)) AS geometrie,
+  gemarkung_name,
+  gemeinde_name,
+  flur,
+  zaehler,
+  nenner,
+  flurstuecksnummer,
+  flurstueckskennzeichen,
+  to_jsonb(flurstuecke) - 'geometrie' AS json
+FROM ${DBSCHEMA}.flurstuecke
+) TO STDOUT WITH CSV HEADER;
+END
+)"
+
+pg2csv $CSV_OUTDIR/flurstuecke_historisch_hro.csv "$(cat << END
+COPY (SELECT
+  uuid AS id,
+  ST_AsText(ST_Buffer(ST_Simplify(geometrie, 0.5), 0)) AS geometrie,
+  gemarkung_name,
+  gemeinde_name,
+  flur,
+  zaehler,
+  nenner,
+  flurstuecksnummer,
+  flurstueckskennzeichen,
+  to_jsonb(flurstuecke_historisch) - 'geometrie' AS json
+FROM ${DBSCHEMA}.flurstuecke_historisch
+) TO STDOUT WITH CSV HEADER;
+END
+)"
+
 pg2csv $CSV_OUTDIR/schulen.csv "$(cat << END
 COPY (SELECT
   uuid AS id,
