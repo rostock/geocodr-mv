5b0450baf2b6c22b8fbfd9c9dc8664f722cb6f55
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2026-01-06 15:51:14 +0100

    cleaned scripts

--- scripts/geocodr-reindex.sh
+++ scripts/geocodr-reindex.sh
@@ -8,9 +8,24 @@ set -u # strict mode
 #
 # % CSV_OUTDIR=/tmp/geocodr-csv $CSV_INDIR=/tmp/geocodr-csv PGHOST=server PGDATABASE=data PGUSER=dbuser PGPASSWORD=pw DBSCHEMA=public geocodr-reindex.sh
 
-/usr/local/geocodr-mv/geocodr-mv/scripts/geocodr-pg2csv-02.sh
+/usr/local/geocodr-mv/geocodr-mv/scripts/geocodr-pg2csv.sh
 
 cat <<EOF | xargs -I % /usr/local/geocodr-mv/virtualenv/bin/geocodr-post --url http://localhost:8983/solr --csv $CSV_INDIR/%.csv --collection %
-orka-app
+flurstueckseigentuemer
+schulen
+adressen
+fluren
+gemarkungen
+gemeinden
+gemeindeteile
+strassen
+flurstuecke
+adressen_hro
+fluren_hro
+gemarkungen_hro
+gemeindeteile_hro
+strassen_hro
+flurstuecke_hro
+postleitzahlengebiete
 EOF
 
